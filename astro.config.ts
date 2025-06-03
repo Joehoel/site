233a613cbbed439e8454aaf860aeb965a1c215e9
Joël Kuijper <jmkuijper@outlook.com>
2025-06-03 22:30:14 +0200

    chore: wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro";
@@ -10,8 +11,8 @@ import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import { defineConfig, envField } from "astro/config";
 // Remark plugins
-import remarkDirective from "remark-directive";/* Handle ::: directives as nodes */
-import { remarkAdmonitions } from "./src/plugins/remark-admonitions";/* Add admonitions */
+import remarkDirective from "remark-directive"; /* Handle ::: directives as nodes */
+import { remarkAdmonitions } from "./src/plugins/remark-admonitions"; /* Add admonitions */
 import { remarkReadingTime } from "./src/plugins/remark-reading-time";
 import { expressiveCodeOptions } from "./src/site.config";
 import { siteConfig } from "./src/site.config";
@@ -25,121 +26,123 @@ import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel";
 
+const isVercelBuild = process.env.CI || process.env.VERCEL;
+
 // https://astro.build/config
 export default defineConfig({
-  image: {
-      domains: ["webmention.io"],
+	image: {
+		domains: ["webmention.io"],
 	},
 
-  integrations: [
-      expressiveCode(expressiveCodeOptions),
-      icon(),
-      tailwind({
-          applyBaseStyles: false,
-          nesting: true,
-      }),
-      sitemap(),
-      mdx(),
-      robotsTxt(),
-      webmanifest({
-          // See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
-          /**
-           * required
-           **/
-          name: siteConfig.title,
-          /**
-           * optional
-           **/
-          description: siteConfig.description,
-          lang: siteConfig.lang,
-          icon: "public/icon.svg", // the source for generating favicon & icons
-          icons: [
-              {
-                  src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
-                  sizes: "180x180",
-                  type: "image/png",
-              },
-              {
-                  src: "icons/icon-192.png",
-                  sizes: "192x192",
-                  type: "image/png",
-              },
-              {
-                  src: "icons/icon-512.png",
-                  sizes: "512x512",
-                  type: "image/png",
-              },
-          ],
-          start_url: "/",
-          background_color: "#1d1f21",
-          theme_color: "#2bbc8a",
-          display: "standalone",
-          config: {
-              insertFaviconLinks: false,
-              insertThemeColorMeta: false,
-              insertManifestLink: false,
-          },
-      }),
-      db(),
-      react(),
-      markdoc(),
-      keystatic(),
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		icon(),
+		tailwind({
+			applyBaseStyles: false,
+			nesting: true,
+		}),
+		sitemap(),
+		mdx(),
+		robotsTxt(),
+		webmanifest({
+			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
+			/**
+			 * required
+			 **/
+			name: siteConfig.title,
+			/**
+			 * optional
+			 **/
+			description: siteConfig.description,
+			lang: siteConfig.lang,
+			icon: "public/icon.svg", // the source for generating favicon & icons
+			icons: [
+				{
+					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+					sizes: "180x180",
+					type: "image/png",
+				},
+				{
+					src: "icons/icon-192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "icons/icon-512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+			],
+			start_url: "/",
+			background_color: "#1d1f21",
+			theme_color: "#2bbc8a",
+			display: "standalone",
+			config: {
+				insertFaviconLinks: false,
+				insertThemeColorMeta: false,
+				insertManifestLink: false,
+			},
+		}),
+		db(),
+		react(),
+		markdoc(),
+		keystatic(),
 	],
 
-  markdown: {
-      rehypePlugins: [
-          [
-              rehypeExternalLinks,
-              {
-                  rel: ["nofollow, noreferrer"],
-                  target: "_blank",
-              },
-          ],
-          rehypeUnwrapImages,
-      ],
-      remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
-      remarkRehype: {
-          footnoteLabelProperties: {
-              className: [""],
-          },
-      },
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					rel: ["nofollow, noreferrer"],
+					target: "_blank",
+				},
+			],
+			rehypeUnwrapImages,
+		],
+		remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: [""],
+			},
+		},
 	},
 
-  // https://docs.astro.build/en/guides/prefetch/
-  prefetch: true,
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
 
-  site: "https://joelkuijper.me/",
+	site: "https://joelkuijper.me/",
 
-  vite: {
-      optimizeDeps: {
-          exclude: ["@resvg/resvg-js"],
-      },
-      plugins: [rawFonts([".ttf", ".woff"])],
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+		plugins: [rawFonts([".ttf", ".woff"])],
 	},
 
-  env: {
-      schema: {
-          WEBMENTION_API_KEY: envField.string({ context: "server", access: "secret", optional: true }),
-          WEBMENTION_URL: envField.string({ context: "client", access: "public", optional: true }),
-          WEBMENTION_PINGBACK: envField.string({ context: "client", access: "public", optional: true }),
-      },
+	env: {
+		schema: {
+			WEBMENTION_API_KEY: envField.string({ context: "server", access: "secret", optional: true }),
+			WEBMENTION_URL: envField.string({ context: "client", access: "public", optional: true }),
+			WEBMENTION_PINGBACK: envField.string({ context: "client", access: "public", optional: true }),
+		},
 	},
 
-  adapter: vercel()
+	adapter: isVercelBuild ? vercel() : node({ mode: "standalone" }),
 });
 
 function rawFonts(ext: string[]) {
-    return {
-        name: "vite-plugin-raw-fonts",
-        // @ts-expect-error:next-line
-        transform(_, id) {
-            if (ext.some((e) => id.endsWith(e))) {
-                const buffer = fs.readFileSync(id);
-                return {
-                    code: `export default ${JSON.stringify(buffer)}`,
-                    map: null,
-                };
-            }
-        },
-    };
+	return {
+		name: "vite-plugin-raw-fonts",
+		// @ts-expect-error:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
 }
